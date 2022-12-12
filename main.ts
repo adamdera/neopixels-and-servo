b2993a3eb4128c4ff4a32d484d4d5c569c355379
Adam De Rango <113118048+adamdera@users.noreply.github.com>
2022-12-12 09:40:28 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,16 +5,28 @@ strip.clear()
 basic.forever(function () {
     if (distance_to_object > 4) {
         strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
-    }
-    if (distance_to_object <= 1) {
-        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+        strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+        strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
+        strip.show()
+        strip.clear()
     }
     if (distance_to_object <= 4) {
         strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
+        strip.show()
+        strip.clear()
     }
-    strip.show()
-    basic.pause(5000)
-    strip.clear()
+    if (distance_to_object <= 1) {
+        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+        strip.show()
+        strip.clear()
+    }
 })
 basic.forever(function () {
     distance_to_object = sonar.ping(
